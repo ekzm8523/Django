ec03662181e313845b935162feb2b6c7a878bea2
JaewonMin <mjw8523@gmail.com>
2021-08-10 15:50:38 +0900

    fix_bug

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from blog.models import Post
 
 class PostLV(ListView):
     model = Post
-    template_name = 'post_all.html'    # default 는 blog/post_list.html 이다.
+    template_name = 'blog/post_all.html'  # default 는 blog/post_list.html 이다.
     context_object_name = 'posts'   # 템플릿 파일로 넘겨주는 객체 리스트에 대한 컨텍스트 변수명을 'posts'로 지정
     paginate_by = 2
 
@@ -20,7 +20,7 @@ class PostDV(DetailView):
 
 class PostAV(ArchiveIndexView):
     model = Post
-    template_name = 'post_archive.html'
+    template_name = 'blog/post_archive.html'
     date_field = 'modify_dt'
 
 
